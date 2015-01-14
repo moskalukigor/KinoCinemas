e8beee8af6095619024a3f130c7a7e685de55073
moskalukigor <moskalukigor@mail.ru>
2015-01-14 20:20:47 +0200

    Upd1

---
+++ Abstract/AbstractManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cinemas
+{
+
+    public class AbstractManager
+    {
+        private readonly string _connectionString;
+    
+        public AbstractManager(string connectionString)
+        {
+            this._connectionString = connectionString;
+        }
+    
+        protected DbContext CreateDbContext()
+        {
+            return new DbContext(_connectionString);
+        }
+    }
+
+}
---
+++ Abstract/CinemasManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cinemas.Abstract
+{
+    class CinemasManager : AbstractManager
+    {        
+        public CinemasManager(string connectionString) : base(connectionString) { }
+
+        public IEnumerable<Cinemas> GetAll()
+        {
+            using (DbContext ctx = this.CreateDbContext())
+            {
+                return ctx.Set<Cinemas>().ToList();
+            }
+        }
+
+        public void Add(Cinemas Item)
+        {
+            using (var ctx = this.CreateDbContext())
+            {
+                ctx.Set<Cinemas>().Add(Item);
+                ctx.SaveChanges();
+            }
+        }
+    }
+}
