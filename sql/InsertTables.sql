b093f76e97a07c053fe05a898713cedfb7eba7e2
moskalukigor <moskalukigor@mail.ru>
2015-01-14 19:56:14 +0200

    AddSqlFiles

---
+++ sql/InsertTables.sql
@@ -0,0 +1,9 @@
+insert into Cinemas (Name)values 
+('Україна'),
+('Ера');
+select * from Cinemas
+
+insert into Movie (Name, Genre, Price, Lenght, CinemaId) values
+('Аватар', 'Екшн', 25, 130.5, 1),
+('Футурама', 'Мультики', 25, 25.0, 2);
+select * from Movie
